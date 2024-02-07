9e6ef87740f18cc3dcd8c0b65ee738b18cbe7083
hawwyw <hw21122@my.westlake.school.nz>
2024-02-08 12:47:54 +1300

    aa

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,17 @@
-print("Hello WORLD")
+x = 1
+for x in range (1, 6):
+    print(x)
+    x += 1
+
+y = 1
+while y <= 5:
+    print(y)
+    y += 1
+
+print("1\n2\n3\n4\n5")
+
+
+fruit = ["durian", "nam nam", "dragonfruit"]
+
+for i in fruit:
+    print(i)
