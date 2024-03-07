e8407a1faa92be7345910002c9b1f4b711617f43
Your Name <you@example.com>
2024-03-07 16:04:40 +1300

    aa

--- movietheatre.py
+++ movietheatre.py
@@ -32,7 +32,6 @@ def menu():
 
         get_input = options[selection]()
 
- 
 
 def leave():
     return None
@@ -65,21 +64,34 @@ def search():
 def add_movies():
     movie = enterbox("What movie do you want to add? : ")
     if movie in movies:
-        sure = choicebox("Movie already being shown - do you want to change details?", "Movie Theatre", choices = ("Yes", "No"))
-        if sure == "Yes":
-            edit()
-        else:
-            msgbox("No details changed.")
-    else:
-        msg = "Please fill out movie details below: "
-        title = "Movie Theatre"
-        field_names = ["Genre : ", "Duration : ", "Showtime : ", "Tickets sold : "]
-        field_values = []
-        field_values = multenterbox(msg,title, field_names)
+        sure = buttonbox("Movie already being shown - do you want to change details?", "Movie Theatre", choices = ("Yes", "No"))
+        if sure == "No":
+            return True
+    msg = "Please fill out movie details below: "
+    title = "Movie Theatre"
+    field_names = ["Genre : ", "Duration(minutes) : ", "Showtime : ", "Tickets sold : "]
+    answer = []
+    run = True
+    while run:
+        answer = multenterbox(msg,title, field_names)
+        run = False
+        if not answer[3].isdigit() or not answer[2].isdigit() or not answer[1].isdigit():
+            msgbox("Enter an integer for duration, showtime and tickets sold - use 24 hour time eg 700 for 7am or 1300 for 1 PM")
+            run = True
+        elif int(answer[2]) > 2400 or int(answer[2]) < 0:
+            msgbox("Enter a number between 0 and 2400.")
+            run = True
+        elif int(answer[3]) > 150:
+            msgbox("Max tickets is 150")
+            run = True
+    temp = {"Genre": answer[0], "Duration": answer[1], "Showtime": answer[2], "Tickets sold": answer[3]}
+    movies[movie] = temp
+    return True
+
 
 
 def edit():
     pass
 
-# Main Menu
+# Main program
 menu()
