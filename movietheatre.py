8444e69c495c6d333b43ec8f5816c2413771d1ce
hawwyw <hw21122@my.westlake.school.nz>
2024-03-12 14:20:13 +1300

    aaaaa

--- movietheatre.py
+++ movietheatre.py
@@ -14,7 +14,6 @@ def menu():
         "Find movie showing": search,
         "Update": edit,
         "List movies": print_dict,
-        "Add movies": add_movies,
         "Exit": leave
     }
 
@@ -32,20 +31,19 @@ def menu():
 
         get_input = options[selection]()
 
-
-
 def leave():
     return None
 
 def print_dict():
-    out_string = ""
-    for key in movies:
-        print(key)
-        temp = f"{key}: Genre : {movies[key]['Genre']}, Duration : {movies[key]['Duration']}, Showtime : {movies[key]['Showtime']}, Tickets sold : {movies[key]['Tickets sold']}\n"
+    output = ["\n\n***Movie Details***\n\n"]
+    for movie, details in movies.items():
+        output.append(f"\nMovie: {movie}\n")
+        for key, value in details.items():
+            output.append(f"\t{key}: {value}")
+        
+    msgbox("\n".join(output))
 
-        out_string += temp
-    
-    msgbox(out_string)
+
 
 def search():
     choices = []
@@ -53,23 +51,103 @@ def search():
         choices.append(i)
     selection = choicebox("Select a movie to see details: ", "Search Movies", choices)
     
-    genre = movies[selection]['Genre']
-    duration = movies[selection]['Duration']
-    showtime = movies[selection]['Showtime']
-    tickets_sold = movies[selection]['Tickets sold']
-
-    msg = ("Genre : ", genre, "\n", "Duration : ", duration, "\n", "Showtime : ", showtime, "\n", "Tickets sold : ", tickets_sold)
+    details = movies[selection]
+    output = [f"\n\n***Movie Details***\n\n{selection}\n"]
+    for key, value in details.items():
+        output.append(f"\t{key}: {value}")
+    msg = ("\n".join(output))
 
     msgbox(msg, "Search Movies")
+    return True
 
 def add_movies():
-    pass
+    movie = enterbox("What movie do you wish to add?")
+    if movie in movies:
+        sure = buttonbox(f"{movie} is already scheduled to show. Do you wish to change the details?", "Movie already showing", choices = ("Yes", "No"))
+        if sure == "Yes":
+            edit()
+            return True
+        else:
+            msgbox("Nothing changed", "Movie Theatre")
+    else:
+        running = True
+        input_list = ["Genre", "Duration", "Showtime ( 24 hour time, EG 900 for 9 am or 1500 or 3pm", "Tickets sold(150 max)"]
+        while running:
+            user_in = multenterbox("Enter movie details", "Movie details", input_list)
+            running = False
+            if not user_in:
+                msgbox("Enter something", "Error")
+                user_in = []
+                running = True
 
-def check_exists():
-    pass
+            if "" in user_in:
+                msgbox("Enter something", "Error")
+                user_in = []
+                running = True
+            
+            if not user_in[3].isdigit() or not user_in[2].isdigit() or not user_in[1].isdigit():
+                msgbox("Enter number for 2, 3, 4", "Error")
+                user_in = []
+                running = True
+
+            if int(user_in[3]) > 150 or int(user_in[3]) < 0:
+                msgbox("Enter number between 0 and 150 for tickets sold", "Error")
+                user_in = []
+                running = True
+
+            if int(user_in[1]) <= 0:
+                msgbox("Enter a time above 0 for tickets sold", "Error")
+                user_in = []
+                running = True
+            
+            if int(user_in[2]) > 2400 or int(user_in[2]) < 0:
+                msgbox("Enter number between 0 and 2400 for time", "Error")
+                user_in = []
+                running = True
+
+
+        temp = {
+            "Genre": user_in[0],
+            "Duration": int(user_in[1]),
+            "Showtime": int(user_in[2]),
+            "Tickets sold": int(user_in[3])
+        }
+
+        movies[movie] = temp
+
+        print_dict()
+        return True
 
 def edit():
-    pass
+    movie = choicebox("What movie do you wish to edit?", "Edit details", movies)
+    running = True
+    while running:
+        change = buttonbox("What part of the movie do you wish to change?", "Edit details", choices = ("Genre", "Duration", "Showtime", "Tickets sold", "Nothing, I'm done editing movies"))
+        if change == "Nothing, I'm done editing movies":
+            output = [f"\n\n***Movie Details***\n\n{movie}\n"]
+            for key, value in movies[movie].items():
+                output.append(f"\t{key}: {value}")
+            msg = ("\n".join(output))
+
+            msgbox(msg, "Search Movies")
+            return True
+        else:
+            running1 = True
+            while running1:
+                temp = enterbox(f"Enter the new details for {change}", "Update details")
+                running1 = False
+                if change != "Genre" and not temp.isdigit():
+                    msgbox("Enter a number", "Error")
+                    running1 = True
+                
+                if change == None:
+                    False
+                
+
+
+
+            movies[movie][change] = temp
+            
 
 # Main Menu
 menu()
